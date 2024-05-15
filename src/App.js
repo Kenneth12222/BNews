440d81a67f0e950b8fd4b15b8fa39288c1ae0039
Kenneth Mburu <kenneth.mburu@student.moringaschool.com>
2024-05-15 08:46:10 +0300

    remove

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import AboutMe from './components/AboutMe';
+
 import BlogDetail from './components/BlogDetail';
 import Footer from './components/Footer';
 
@@ -12,8 +11,7 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<HeroSection />} />
-          <Route path="/about" element={<AboutMe />} />
+        
           <Route path="/blog/:id" element={<BlogDetail />} />
           <Route path="/blog" element={<BlogDetail />} />
           <Route path="*" element={<div>Page not found</div>} />
